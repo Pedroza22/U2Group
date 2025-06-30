b93702546936a3aa0a0fb02326ab4c934ba0b90e
Pedroza22 <julianpedrozaospina@gmail.com>
2025-06-29 19:37:22 -0500

    delete front and create the back for the home

--- home/views.py
+++ home/views.py
@@ -1,8 +1,23 @@
 from rest_framework import viewsets
 from .models import Blog, Project
-from .serializer import BlogSerializer, ProjectSerializer
+from .serializer import BlogSerializer, PriceCalculatorSerializer, ProjectSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from .services import calculate_price
+from rest_framework.views import APIView
+
+class PriceCalculatorView(APIView):
+        def post(self, request):
+                serializer = PriceCalculatorSerializer(data=request.data)
+                if serializer.is_valid():
+                        data = serializer.validated_data
+                        total_price = calculate_price(
+                                num_pages=data['num_pages'],
+                                design_level=data['design_level'],
+                                has_multilanguage=data['has_multilanguage']
+                        )
+                        return Response({'total_price': total_price})
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class BlogViewSet(viewsets.ReadOnlyModelViewSet):
         queryset = Blog.objects.all().order_by('-date')
